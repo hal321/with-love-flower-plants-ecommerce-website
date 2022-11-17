0fa1b639d2bb87ed610708b615bc4908d50387d9
hal321 <ibtehalyahia123@gmail.com>
2022-11-17 16:14:33 +0800

    port

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-import express from 'express'
-import session from 'express-session'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import morgan from 'morgan'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import path from 'path'
-import flowersController from './controllers/flower.js'
-import indoorController from './controllers/indoor.js'
-import outdoorController from './controllers/outdoor.js'
-import usersController from './controllers/user.js'
-import reviewController from './controllers/reviews.js'
-import loginController from './controllers/login.js'
-const PORT = process.env.PORT || 8080
-const app = express()
-dotenv.config()
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('dev'))
+import express from "express";
+import session from "express-session";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+import flowersController from "./controllers/flower.js";
+import indoorController from "./controllers/indoor.js";
+import outdoorController from "./controllers/outdoor.js";
+import usersController from "./controllers/user.js";
+import reviewController from "./controllers/reviews.js";
+import loginController from "./controllers/login.js";
+const PORT = process.env.PORT || 10000;
+const app = express();
+dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
 }
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 mongoose
   .connect(process.env.MONGODB_URI, {
@@ -26,27 +26,27 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => {
-      console.log('listening on port no', PORT)
-    })
+      console.log("listening on port no", PORT);
+    });
   })
-  .catch(console.error)
+  .catch(console.error);
 
-app.use(express.static(path.resolve(__dirname, './client/build')))
-app.use(express.json())
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.json());
 app.use(
   session({
-    secret: 'gotasecertcanukeepit',
+    secret: "gotasecertcanukeepit",
     resave: false,
     saveUninitialized: false,
   })
-)
-app.use('/flowers', flowersController)
-app.use('/indoor', indoorController)
-app.use('/outdoor', outdoorController)
-app.use('/users', usersController)
-app.use('/review', reviewController)
-app.use('/login', loginController)
+);
+app.use("/flowers", flowersController);
+app.use("/indoor", indoorController);
+app.use("/outdoor", outdoorController);
+app.use("/users", usersController);
+app.use("/review", reviewController);
+app.use("/login", loginController);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
